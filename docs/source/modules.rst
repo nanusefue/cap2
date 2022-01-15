a5d57d143d921b4ba2d151c75f511679579fbe9f
Esteban Martin <esteban.martin@alten.es>
2022-01-15 16:50:57 +0100

    first commit #1

---
+++ docs/source/modules.rst
@@ -0,0 +1,22 @@
+Modules
+=======
+
+The CAP divides modules into five subpipelines covering: quality control, preprocessing to remove low quality and contaminat sequences, analysis of short reads, analysis of assemblies, and constructing relevant databases and indices for the other four pipelines. Detailed descriptions for each subpipeline and the modules within may be found below. 
+
+Each link contains a description of the subpipeline, and a list of modules in the pipelines with descriptions and links to example outputs from each module.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Documentation:
+
+   Quality Control <modules_qc>
+   Preprocessing <modules_preprocessing>
+   Short Read <modules_short_read>
+   Assembly <modules_assembly>
+   Databases <modules_databases>
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+
+import setuptools
+
+setuptools.setup(
+    name='cap2',
+    version='0.4.1',
+    description="CAP2",
+    author="David C. Danko",
+    author_email='dcdanko@gmail.com',
+    url='https://github.com/metasub/cap2',
+    packages=setuptools.find_packages(),
+    package_dir={'cap2': 'cap2'},
+    install_requires=[
+        'pandas==1.3.4,<2.0',
+        'numpy',
+        'scipy==1.7.3,<2.0',
+        'python-daemon>=2.3.0,<3.0',
+        'luigi>=3.0.3,<3.1',
+        'Pillow==8.4.0',
+        'PyYaml==6.0',
+        'biopython==1.76',
+        'click>=6.0',
+        'Jinja2==3.0.3',  # for multiqc
+        'multiqc==1.11',  # hackish, tbd if I'm okay with this
+        'pangea_api>=0.9.24,<1.0',
+        'pysam==0.18.0',
+        'python-louvain==0.15',
+        'gimmebio.seqs==0.9.3',
+        'bloom_filter>=1.3.3,<2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cap2=cap2.cli:main',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
